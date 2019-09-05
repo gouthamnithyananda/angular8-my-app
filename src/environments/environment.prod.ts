85282db7430bf855fdebd68de050d9dc3726c380
Goutham Nithyananda <goutham9964@gmail.com>
2019-09-05 11:13:12 +0530

    removed api key and db url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiKey: "AIzaSyCHW1otlkWvnjAkm-D2WzgD5GjO-fCBXtM",
-  databaseURL: "https://my-app-2d49f.firebaseio.com",
+  apiKey: "xxx",
+  databaseURL: "xxxx",
   production: true
 };
